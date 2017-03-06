168531e93450008bfa9e37c34b29b494351c6e0c
JonSteinn <jonsteinn@gmail.com>
2017-03-06 15:46:03 +0000

    some gui tests and improvements

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -1,6 +1,8 @@
 package gui;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
@@ -13,40 +15,47 @@ import level.RandomBoardGenerator;
 public class Main extends Application {
 
     RandomBoardGenerator generator;
-    Board board;
-    Button[][] buttons;
-    Button generato;
+    BoardButtons mines;
+    Button generate;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
 
 
         generator = new RandomBoardGenerator();
-        board = generator.create(10, 10, 30, false, false);
+        mines = new BoardButtons(generator.create(6, 6, 10, false, false));
 
         GridPane pane = new GridPane();
-        buttons = new Button[10][10];
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
-                buttons[i][j] = new Button(board.containsBomb(i,j) ? "X" : "O");
-                pane.add(buttons[i][j], i, j);
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 6; j++) {
+                pane.add(mines.getButton(i, j), i, j);
             }
         }
+        pane.setPadding(new Insets(0,0,10,0));
 
         BorderPane bPane = new BorderPane();
         bPane.setCenter(pane);
-        generato = new Button("Generate");
-        generato.setOnMouseClicked((event) -> {
-            board = generator.create(10,10,30, false, false);
-            for (int i = 0; i < 10; i++) {
-                for (int j = 0; j < 10; j++) {
-                    buttons[i][j].setText(board.containsBomb(i,j) ? "X" : "O");
+        generate = new Button("Generate");
+        generate.setOnMouseClicked((event) -> {
+            pane.getChildren().remove(0, 36);
+            mines = new BoardButtons(generator.create(6,6, 10, false, false));
+            for (int i = 0; i < 6; i++) {
+                for (int j = 0; j < 6; j++) {
+                    pane.add(mines.getButton(i, j), i, j);
                 }
             }
         });
-        bPane.setBottom(generato);
+        bPane.setBottom(generate);
+        bPane.setPadding(new Insets(5,5,5,5));
+        bPane.setAlignment(generate, Pos.CENTER);
+        bPane.setAlignment(pane, Pos.CENTER);;
 
-        Scene scene = new Scene(bPane,550 ,450, Color.LIGHTGRAY);
+        BorderPane ghost = new BorderPane();
+        ghost.setCenter(bPane);
+        ghost.setPadding(new Insets(5,5,5,5));
+        ghost.setAlignment(bPane, Pos.CENTER);
+
+        Scene scene = new Scene(ghost);
 
         primaryStage.setTitle("Minesweeper");
         primaryStage.setScene(scene);
--- test/level/BoardTest.java
+++ test/level/BoardTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 
 import java.util.Random;
 
+import static com.sun.xml.internal.ws.dump.LoggingDumpTube.Position.Before;
 import static org.junit.Assert.*;
 
 /**
